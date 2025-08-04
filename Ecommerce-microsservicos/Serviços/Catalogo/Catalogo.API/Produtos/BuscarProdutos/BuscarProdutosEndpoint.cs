0da678323fa726d298845d06dcc9063975250b97
Felipe eu mesmo <luiz_sena@hotmail.com>
2025-08-03 23:47:40 -0300

    adicionando get

---
+++ Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/BuscarProdutos/BuscarProdutosEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalogo.API.Produtos.BuscarProdutos
+{
+    // public record BuscarProdutosRequest();
+    public record BuscarProdutosResponse(IEnumerable<Produto> Produtos);
+    public class BuscarProdutosEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/produtos", async (ISender sender) => {
+                var resultado = await sender.Send(new BuscarProdutosQuery());
+                var resposta = resultado.Adapt<BuscarProdutosResponse>();
+                return Results.Ok(resposta);
+            })
+            .WithName("BuscarProdutos")
+            .Produces<BuscarProdutosResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Buscar produtos")
+            .WithDescription("Buscar produtos")
+            ;
+        }
+    }
+}
---
+++ Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/BuscarProdutos/BuscarProdutosQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Catalogo.API.Produtos.BuscarProdutos
+{
+    public record BuscarProdutosQuery() : IQuery<BuscarProdutosResult>;
+    public record BuscarProdutosResult(IEnumerable<Produto> Produtos);
+
+    public class BuscarProdutosQueryHandler(IDocumentSession session, ILogger<BuscarProdutosQueryHandler> logger)
+        : IQueryHandler<BuscarProdutosQuery, BuscarProdutosResult>
+    {
+        public async Task<BuscarProdutosResult> Handle(BuscarProdutosQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("BuscarProdutosQueryHandler.Handle chamado com a query {@Query}", query);
+            var produtos = await session.Query<Produto>().ToListAsync(cancellationToken);
+            return new BuscarProdutosResult(produtos);
+        }
+    }
+}

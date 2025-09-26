4f398fa693cb42c2c611feeb62906105678e5228
Felipe eu mesmo <luiz_sena@hotmail.com>
2025-09-26 01:15:35 -0300

    adicionando coisa

---
+++ Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/BuscarProdutoPorCategoria/BuscarProdutosPorCategoriaEndpoint.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Catalogo.API.Produtos.BuscarProdutoPorCategoria
+{
+    //public record BuscarProdutosPorCategoriaRequest();
+    public record BuscarProdutosPorCategoriaResponse(IEnumerable<Produto> produtos);
+
+    public class BuscarProdutosPorCategoriaEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/produtos/categoria/{categoria}",
+                async (string categoria, ISender sender) => {
+                    var result = await sender.Send(new BuscarProdutosPorCategoriaQuery(categoria));
+                    var response = result.Adapt<BuscarProdutosPorCategoriaResponse>();
+                    return Results.Ok(response);
+                })
+                .WithName("BuscarProdutosPorCategoria")
+                .Produces<BuscarProdutosPorCategoriaResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Buscar produtos por categoria")
+                .WithDescription("Buscar produtos pro categoria");
+        }
+    }
+}
---
+++ Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/BuscarProdutoPorCategoria/BuscarProdutosPorCategoriaQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Catalogo.API.Produtos.BuscarProdutoPorCategoria
+{
+    public record BuscarProdutosPorCategoriaQuery(string Categoria) : IQuery<BuscarProdutoPorCategoriaResult>;
+    public record BuscarProdutoPorCategoriaResult(IEnumerable<Produto> Produtos);
+
+    internal class BuscarProdutosPorCategoriaQueryHandler
+        (IDocumentSession session, ILogger<BuscarProdutosPorCategoriaQueryHandler> logger)
+        :
+        IQueryHandler<BuscarProdutosPorCategoriaQuery, BuscarProdutoPorCategoriaResult>
+    {
+        public async Task<BuscarProdutoPorCategoriaResult> Handle(BuscarProdutosPorCategoriaQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("BuscarProdutoPorCategoriaQueryHandler.Handle chamado com {@Query}", query);
+
+            var produtos = await session.Query<Produto>()
+                .Where(c => c.Categorias.Contains(query.Categoria))
+                .ToListAsync();
+
+            return new BuscarProdutoPorCategoriaResult(produtos);
+        }
+    }
+}

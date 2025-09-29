e3f4d0d817f94eb5ac3fa3b91436a2c812a66370
Felipe eu mesmo <luiz_sena@hotmail.com>
2025-09-29 01:19:09 -0300

    adicionando coisa

--- Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/AtualizarProduto/AtualizarProdutoCommandHandler.cs
+++ Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/AtualizarProduto/AtualizarProdutoCommandHandler.cs
@@ -1,6 +1,41 @@
-﻿namespace Catalogo.API.Produtos.AtualizarProduto
+﻿
+namespace Catalogo.API.Produtos.AtualizarProduto
 {
-    public class AtualizarProdutoCommandHandler
+    public record AtualizarProdutoComando(
+        Guid Id,
+        string Nome,
+        List<string> Categorias,
+        string Descricao,
+        string ArquivoImagem,
+        decimal Preco) :
+    ICommand<AtualizarProdutoResult>;
+
+    public record AtualizarProdutoResult(bool Sucesso);
+
+    public class AtualizarProdutoCommandHandler(IDocumentSession session, ILogger<AtualizarProdutoCommandHandler> logger)
+        : ICommandHandler<AtualizarProdutoComando, AtualizarProdutoResult>
     {
+        public async Task<AtualizarProdutoResult> Handle(AtualizarProdutoComando request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("AtualizarProdutoCommandHandler.Handle chamado com {@Query}", request);
+
+            var produto = await session.LoadAsync<Produto>(request.Id, cancellationToken);
+
+            if(produto is null)
+            {
+                throw new ProdutoNaoEncontradoException();
+            }
+
+            produto.Nome = request.Nome;
+            produto.Categorias = request.Categorias;
+            produto.Descricao = request.Descricao;
+            produto.ArquivoImagem = request.ArquivoImagem;
+            produto.Preco = request.Preco;
+
+            session.Update(produto);
+            await session.SaveChangesAsync();
+
+            return new AtualizarProdutoResult(true);
+        }
     }
 }
--- Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/AtualizarProduto/AtualizarProdutoEndpoint.cs
+++ Ecommerce-microsservicos/Serviços/Catalogo/Catalogo.API/Produtos/AtualizarProduto/AtualizarProdutoEndpoint.cs
@@ -1,6 +1,32 @@
-﻿namespace Catalogo.API.Produtos.AtualizarProduto
+﻿
+using Catalogo.API.Produtos.BuscarProdutoPorCategoria;
+
+namespace Catalogo.API.Produtos.AtualizarProduto
 {
-    public class AtualizarProdutoEndpoint
+    public record AtualizarProdutoRequest(Guid Id,
+        string Nome,
+        List<string> Categorias,
+        string Descricao,
+        string ArquivoImagem,
+        decimal Preco);
+
+    public record AtualizarProdutoResponse(bool Sucesso);
+    public class AtualizarProdutoEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/produtos", async (AtualizarProdutoRequest request, ISender sender) => {
+                var comando = request.Adapt<AtualizarProdutoCommandHandler>();
+                var result = sender.Send(comando);
+                var resposta = result.Adapt<AtualizarProdutoResponse>();
+
+                return Results.Ok(resposta);
+            })
+                .WithName("AtualizarProduto")
+                .Produces<AtualizarProdutoResponse>(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Atualizar produto")
+                .WithDescription("Atualizar produto!");
+        }
     }
 }

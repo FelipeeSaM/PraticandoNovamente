7045a332ea7eadc1d84db4818bd698b35fba7ecd
Felipe eu mesmo <luiz_sena@hotmail.com>
2026-01-01 17:16:25 -0300

    configurations

--- Ecommerce-microsservicos/Serviços/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
+++ Ecommerce-microsservicos/Serviços/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -1,11 +1,120 @@
 ﻿
+using Ordering.Domain.Enums;
+
 namespace Ordering.Infrastructure.Data.Configurations
 {
     public class OrderConfiguration : IEntityTypeConfiguration<Order>
     {
         public void Configure(EntityTypeBuilder<Order> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasConversion(orderItemId => orderItemId.Value,
+                dbId => OrderId.Of(dbId));
+
+            builder.HasOne<Customer>()
+                .WithMany()
+                .HasForeignKey(c => c.CustomerId)
+                .IsRequired();
+
+            builder.HasMany(c => c.OrderItems)
+                .WithOne()
+                .HasForeignKey(oi => oi.OrderId);
+
+            builder.ComplexProperty(
+                c => c.OrderName, nameBuilder => {
+                    nameBuilder.Property(n => n.Value)
+                        .HasColumnName(nameof(Order.OrderName))
+                        .HasMaxLength(100)
+                        .IsRequired();
+                }
+            );
+
+            builder.ComplexProperty(
+                o => o.ShippingAddress, addressBuilder => {
+                    addressBuilder.Property(a => a.FirstName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addressBuilder.Property(a => a.LastName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addressBuilder.Property(a => a.EmailAddress)
+                    .HasMaxLength(50);
+
+                    addressBuilder.Property(a => a.AddressLine)
+                    .HasMaxLength(180)
+                    .IsRequired();
+
+                    addressBuilder.Property(a => a.Country)
+                    .HasMaxLength(50);
+
+                    addressBuilder.Property(a => a.State)
+                    .HasMaxLength(50);
+
+                    addressBuilder.Property(a => a.ZipCode)
+                    .HasMaxLength(5)
+                    .IsRequired();
+                }
+            );
+
+            builder.ComplexProperty(
+                o => o.BillingAddress, addressBuilder => {
+                    addressBuilder.Property(a => a.FirstName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addressBuilder.Property(a => a.LastName)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+                    addressBuilder.Property(a => a.EmailAddress)
+                    .HasMaxLength(50);
+
+                    addressBuilder.Property(a => a.AddressLine)
+                    .HasMaxLength(180)
+                    .IsRequired();
+
+                    addressBuilder.Property(a => a.Country)
+                    .HasMaxLength(50);
+
+                    addressBuilder.Property(a => a.State)
+                    .HasMaxLength(50);
+
+                    addressBuilder.Property(a => a.ZipCode)
+                    .HasMaxLength(5)
+                    .IsRequired();
+                }
+            );
+
+            builder.ComplexProperty(
+                o => o.Payment, paymentBuilder => {
+                    paymentBuilder.Property(c => c.CardName)
+                    .HasMaxLength(50);
+
+                    paymentBuilder.Property(c => c.CardNumber)
+                    .HasMaxLength(24)
+                    .IsRequired();
+
+                    paymentBuilder.Property(c => c.Expiration)
+                    .HasMaxLength(10);
+
+                    paymentBuilder.Property(c => c.CVV)
+                    .HasMaxLength(3);
+
+                    paymentBuilder.Property(c => c.PaymentMethod);
+                }
+            );
+
+            builder.Property(c => c.Status)
+                .HasDefaultValue(OrderStatusEnum.Draft)
+                .HasConversion(
+                    d => d.ToString(),
+                    dbStatus => (OrderStatusEnum)Enum.Parse(typeof(OrderStatusEnum), dbStatus));
+
+            builder.Property(c => c.TotalPrice);
         }
     }
 }
--- Ecommerce-microsservicos/Serviços/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
+++ Ecommerce-microsservicos/Serviços/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -4,7 +4,18 @@
     {
         public void Configure(EntityTypeBuilder<OrderItem> builder)
         {
-            throw new NotImplementedException();
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasConversion(orderItemId => orderItemId.Value,
+                dbId => OrderItemId.Of(dbId));
+
+            builder.HasOne<Product>()
+                .WithMany()
+                .HasForeignKey(c => c.ProductId);
+
+            builder.Property(c => c.Quantity).IsRequired();
+            builder.Property(c => c.Price).IsRequired();
         }
     }
 }

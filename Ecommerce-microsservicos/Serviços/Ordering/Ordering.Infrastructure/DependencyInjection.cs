836e166a6570275710c8e48ad8b79ff358e97c59
Felipe eu mesmo <luiz_sena@hotmail.com>
2026-01-01 18:15:11 -0300

    migration

--- Ecommerce-microsservicos/Serviços/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ Ecommerce-microsservicos/Serviços/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Ordering.Infrastructure
+﻿namespace Ordering.Infrastructure
 {
     public static class DependencyInjection
     {
         public static IServiceCollection AddInfraStructureServices(this IServiceCollection services, IConfiguration config)
         {
             var connection = config.GetConnectionString("Database");
-            // Register application services here
+
+            services.AddDbContext<ApplicationDbContext>(opt => {
+                opt.UseSqlServer(connection);
+            });
+
             return services;
         }
     }

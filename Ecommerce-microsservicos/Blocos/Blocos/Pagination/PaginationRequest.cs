bbcf754f54556ebbf7f8c24066bc73f6044e1d1a
Felipe eu mesmo <luiz_sena@hotmail.com>
2026-01-04 05:36:19 -0300

    completando application layer

---
+++ Ecommerce-microsservicos/Blocos/Blocos/Pagination/PaginationRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Blocos.Pagination
+{
+    public record PaginationRequest(int PageIndex = 0, int PageSize = 10);
+}
--- Ecommerce-microsservicos/Serviços/Ordering/Ordering.Application/DependencyInjection.cs
+++ Ecommerce-microsservicos/Serviços/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,17 +1,14 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System.Reflection;
-
-namespace Ordering.Application
+﻿namespace Ordering.Application;
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
-        {
-            services.AddMediatR(cfg => {
-                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-            });
+        services.AddMediatR(config => {
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
 
-            return services;
-        }
+        return services;
     }
 }

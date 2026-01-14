dce8082bdb52d0fe5bb844d6cc0ec88cf37322ce
Felipe eu mesmo <luiz_sena@hotmail.com>
2026-01-13 23:47:03 -0300

    add coisa

---
+++ Ecommerce-microsservicos/Serviços/Ordering/Ordering.API/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Carter;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
+global using Blocos.Exceptions.Handler;
+global using Microsoft.Extensions.Configuration;

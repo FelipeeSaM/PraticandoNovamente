e69057f6b218d06a64518728431bee6467ddf985
Felipe eu mesmo <luiz_sena@hotmail.com>
2026-01-22 01:09:28 -0300

    rabitmq e mastransit

--- Ecommerce-microsservicos/Serviços/Ordering/Ordering.Application/DependencyInjection.cs
+++ Ecommerce-microsservicos/Serviços/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,13 +1,21 @@
-﻿namespace Ordering.Application;
+﻿using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
+
+namespace Ordering.Application;
+
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatR(config => {
             config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             config.AddOpenBehavior(typeof(ValidationBehavior<,>));
             config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
+
+        //services.AddFeatureManagement();
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
         return services;
     }
